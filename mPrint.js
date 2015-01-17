5ee9ece029e1f0bfd5dc02614e961cb448ccb01f
Carisa Covins <covins@umich.edu>
2015-01-17 16:27:57 -0500

    First commit

---
+++ mPrint.js
@@ -0,0 +1,9 @@
+$(document).ready( function() {
+
+  $("#sendToP").click( function(){
+    window.alert("ALARMING");
+  });
+  // $('#sendToP').mouseenter( function(){
+     $('numCop').fadeOut('fast', .25);
+  // });
+});
